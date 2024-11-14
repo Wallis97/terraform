91513b68913495f49bb26e95ebc60395e5ca7f96
Adam Wallis <adamwallis97@gmail.com>
2024-11-14 09:17:58 +0100

    Add main.tf file. Update VMs module variables.tf

--- azure/azure-k8s/modules/azure-vm/variables.tf
+++ azure/azure-k8s/modules/azure-vm/variables.tf
@@ -2,3 +2,51 @@ variable "rg_name" {
   type = string
   description = "Resource Group name"
 }
+
+variable "location" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "vnet_ip_range" {
+  type = list(string)
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "subnet_ip_range" {
+  type = list(string)
+}
+
+variable "ip_config_name" {
+  type = string
+}
+
+variable "vm_name" {
+  type = string
+}
+
+variable "vm_size" {
+  type = string
+}
+
+variable "username" {
+  type = string
+}
+
+variable "nic_name" {
+  type = string
+}
+
+variable "sa_type" {
+  type = string
+}
+
+variable "caching" {
+  type = string
+}
